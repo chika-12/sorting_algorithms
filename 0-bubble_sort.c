ee7d4c73e6193f2c4d232847871b63e7b8bd12da
chika-12 <markworship001@gmail.com>
2024-04-27 13:22:52 +0100

    Bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (index = 0; index < size; index++)
 	{
-		for (idx = 0; idx < size - index - 1; idx++)
+		for (idx = 0; idx < size - 1; idx++)
 		{
 			if (array[idx] > array[idx + 1])
 			{
