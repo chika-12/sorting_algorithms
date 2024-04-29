fbde77a4322eee486c27a3b66124083c0151ba15
chika-12 <markworship001@gmail.com>
2024-04-29 05:21:06 +0100

    quick_sort

--- sort.h
+++ sort.h
@@ -23,4 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, size_t size, int low, int high);
+void quicksort_recursion(int *array, size_t size, int low, int high);
+void swap(int *x, int *y);
 #endif
